e6f28dd1732cb25199919cadb6ad76a947416fdf
MathTeacherSmith <Clarence.Smith@vertexacademies.org>
2026-01-29 19:10:56 +0000

    Complete and changed the username/password

--- multi_factor_CS.py
+++ multi_factor_CS.py
@@ -2,11 +2,16 @@
 import multifactorcli as mfc
 
 # create a multi-factor interface to a restricted app
-my_auth = mfc.MultiFactorAuth()
+my_auth = mfc.MultiFactorAuth() # This is a class object
+
+# set the username and password
+username = "banana"
+password = "apple"
+my_auth.set_authetication(username, password)
 
 # set the users authentication information
 question = "What is your favorite color?"
-answer = "purple"
+answer = "red"
 my_auth.set_multiFactorAuthentication(question, answer)
 
 my_auth.run()
